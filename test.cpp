4633be8e47b8b2cc894165e37f101761aecdc4a0
Gihan sudeepa <86099252+GIHAA@users.noreply.github.com>
2022-01-06 15:52:42 +0530

    Update test.cpp

--- test.cpp
+++ test.cpp
@@ -1,1 +1,4 @@
-
+#define FIREBASE_HOST "" // Firebase host
+#define FIREBASE_AUTH "" //Firebase Auth code
+#define WIFI_SSID "" //Enter your wifi Name eg-Dialog 4g 734
+#define WIFI_PASSWORD "" //Enter wifi password
